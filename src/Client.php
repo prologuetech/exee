2e1d5be85dc9e51b8ebc7ff2bd8ea30147c16947
Eric Percifield <epercifield@prologuetechnology.com>
2017-11-10 14:23:03 -0600

    Test react client.

--- src/Client.php
+++ src/Client.php
@@ -1,6 +1,25 @@
 <?php
 namespace Prologuetech\Exee;
 
+use React\EventLoop\Factory;
+use React\Socket\Connector;
+use React\Socket\ConnectionInterface;
+
 class Client {
 
+	public function open() {
+		$target = 'localhost:2000';
+		$loop = Factory::create();
+		$connector = new Connector($loop);
+		$connector->connect($target)->then(function (ConnectionInterface $connection) use ($target) {
+			$connection->on('data', function ($data) {
+				echo $data;
+			});
+			$connection->on('close', function () {
+				echo '[CLOSED]' . PHP_EOL;
+			});
+			$connection->write('0,"019"1,"Service Availability"23,"1"50,"US"117,"US"99,""');
+		}, 'printf');
+		$loop->run();
+	}
 }
