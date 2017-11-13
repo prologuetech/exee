e26f9058496fc0ca14caca3f8004eecc0f28b265
Eric Percifield <epercifield@prologuetechnology.com>
2017-11-13 15:39:15 -0600

    Add OpenshipRate model.

---
+++ src/models/OpenshipRate.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Prologuetech\Exee\Models;
+
+use Prologuetech\Exee\FieldTypes;
+use Prologuetech\Exee\Model;
+
+class OpenshipRate extends Model
+{
+	/**
+	 * Required fields for model
+	 *
+	 * @var array
+	 */
+	public static $requiredFields = [
+		FieldTypes::OPENSHIP_FLAGS,
+		FieldTypes::OPENSHIP_INDEX,
+		FieldTypes::RATE_FLAG,
+		FieldTypes::NO_SHIPTIME_RATES,
+		FieldTypes::RATE_QUOTE_TYPE,
+	];
+}
