2e478b7565a95e6336f07374c99200b1f60f0577
Eric Percifield <epercifield@prologuetechnology.com>
2017-11-13 15:38:17 -0600

    Add openship flag handlers.

---
+++ src/Openship.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Prologuetech\Exee;
+
+class Openship
+{
+	/**
+	 * Bit flag for on/true
+	 */
+	const ON = 'Y';
+
+	/**
+	 * Bit flag for off/false
+	 */
+	const OFF = 'N';
+
+	/**
+	 * Transaction positions
+	 */
+	const CREATE_SHIPMENT = 1;
+	const ROUTE_OR_TRANSIT_IN_TIME = 2;
+	const RATE_SHIPMENT = 3;
+	const ADD_PACKAGE = 4;
+	const EDIT_PACKAGE = 5;
+	const EDIT_SHIPMENT = 6;
+	const DELETE_PACKAGE = 7;
+	const DELETE_SHIPMENT = 8;
+	const CONFIRM_SHIPMENT = 9;
+
+	/**
+	 * Toggles flags on for given types
+	 *
+	 * @param array $toggles
+	 * @return string
+	 */
+	public static function flags($toggles)
+	{
+		$flags = [
+			1 => 'N',
+			2 => 'N',
+			3 => 'N',
+			4 => 'N',
+			5 => 'N',
+			6 => 'N',
+			7 => 'N',
+			8 => 'N',
+			9 => 'N',
+		];
+
+		// Loop our flags
+		foreach ($toggles as $position) {
+			// Replace our prior value with static::ON
+			$flags[$position] = static::ON;
+		}
+
+		return implode('', $flags);
+	}
+}
